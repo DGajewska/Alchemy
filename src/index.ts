348f703cfc861c5e66e02ea5059dd00511bb0b36
Dana Gajewska <danusia.x@outlook.com>
2025-11-20 09:06:24 -0500

    Cleans up packages, setup. App running 👌

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import type { Request, Response } from "express";
+
+import express from 'express';
+import cors from 'cors';
+
+const PORT = process.env.PORT || 5050
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.json({ status: 'API is running on /api' });
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
